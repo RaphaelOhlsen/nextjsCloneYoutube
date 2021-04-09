8db1361126faed1307c71f7217427c50000cb08e
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2021-04-09 15:06:14 -0300

    style: materia-UI Configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
-  return <div>Clone Youtube NextJs and MaterialUI</div>;
+  return (
+    <>
+      Clone Youtube NextJs and MaterialUI
+      <Button variant="outlined" color="secondary">
+        Teste
+      </Button>
+    </>
+  );
 }
