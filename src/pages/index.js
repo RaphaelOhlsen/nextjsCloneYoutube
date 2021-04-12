76b8b829e62cedc41171610ec0be26ef3a0dec33
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2021-04-12 18:33:30 -0300

    feat(component): layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <>
+    <Layout title="Youtube">
       Clone Youtube NextJs and MaterialUI
       <Button variant="outlined" color="secondary">
         Teste
       </Button>
-    </>
+    </Layout>
   );
 }
